8604caa6375dc15ae38ec45dc300c0f0fcf43862
Sanjay Kumar <sanjay.kumar@cscs.io>
2025-09-13 18:52:00 +0530

    Initial Commit from Local

---
+++ src/test/java/io/nasa/userManagement/UserManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package io.nasa.userManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
